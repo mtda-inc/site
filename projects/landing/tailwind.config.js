6fa347cb8a19426692c6cc38896611754135ffb4
Matheus Davidson <matheusdavidson@gmail.com>
2022-04-19 17:37:03 -0300

    feat(theme)

--- projects/landing/tailwind.config.js
+++ projects/landing/tailwind.config.js
@@ -1,65 +1,62 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  mode: "jit",
-  content: [
-    "projects/landing/src/**/*.{html,ts,scss}",
-    "./node_modules/ng-tw/**/*",
-  ],
-  options: {
-    safelist: [
-      /^px-/,
-      /^py-/,
-      /^border-/,
-      /^text-/,
-      /^ring-/,
-      /^focus:/,
-      /^bg-primary/,
-      /^hover:bg-primary/,
-      /^bg-secondary/,
-      /^hover:bg-secondary/,
-      /^bg-danger/,
-      /^hover:bg-danger/,
-    ],
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-      sans: ['"Inter"', "sans-serif"],
+    mode: 'jit',
+    content: ['projects/landing/src/**/*.{html,ts,scss}', './node_modules/ng-tw/**/*'],
+    options: {
+        safelist: [
+            /^px-/,
+            /^py-/,
+            /^border-/,
+            /^text-/,
+            /^ring-/,
+            /^focus:/,
+            /^bg-primary/,
+            /^hover:bg-primary/,
+            /^bg-secondary/,
+            /^hover:bg-secondary/,
+            /^bg-danger/,
+            /^hover:bg-danger/,
+        ],
     },
-    extend: {
-      colors: {
-        primary: {
-          ...colors.violet,
-          DEFAULT: opacityColor("109, 40, 217"),
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            sans: ['"Inter"', 'sans-serif'],
         },
-        secondary: {
-          ...colors.blue,
-          DEFAULT: opacityColor("37, 99, 235"),
+        extend: {
+            colors: {
+                primary: {
+                    ...colors.violet,
+                    DEFAULT: opacityColor('109, 40, 217'),
+                },
+                secondary: {
+                    ...colors.lime,
+                    DEFAULT: opacityColor('101, 163, 13'),
+                },
+                danger: {
+                    ...colors.red,
+                    DEFAULT: opacityColor('220, 38, 38'),
+                },
+            },
         },
-        danger: {
-          ...colors.red,
-          DEFAULT: opacityColor("220, 38, 38"),
-        },
-      },
     },
-  },
-  variants: {
-    extend: {
-      opacity: ["disabled"],
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
     },
-  },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
 
 function opacityColor(color) {
-  return ({ opacityVariable, opacityValue }) => {
-    if (opacityValue !== undefined) {
-      return `rgba(${color}, ${opacityValue})`;
-    }
-    if (opacityVariable !== undefined) {
-      return `rgba(${color}, var(${opacityVariable}, 1))`;
-    }
-    return `rgb(${color})`;
-  };
+    return ({ opacityVariable, opacityValue }) => {
+        if (opacityValue !== undefined) {
+            return `rgba(${color}, ${opacityValue})`;
+        }
+        if (opacityVariable !== undefined) {
+            return `rgba(${color}, var(${opacityVariable}, 1))`;
+        }
+        return `rgb(${color})`;
+    };
 }
