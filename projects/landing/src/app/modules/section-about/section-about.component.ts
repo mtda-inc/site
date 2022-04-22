9b0febd7c9d3786a9b97de572c5e1f96c4680e53
Matheus Davidson <matheusdavidson@gmail.com>
2022-04-22 17:47:29 -0300

    feat(about)

--- projects/landing/src/app/modules/section-about/section-about.component.ts
+++ projects/landing/src/app/modules/section-about/section-about.component.ts
@@ -8,7 +8,38 @@ import { IconService } from '../../services/icon.service';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SectionAboutComponent implements OnInit {
-    constructor(public icons: IconService) {}
+    public about: any[] = [
+        {
+            title: '<b>15+ years</b> experience',
+            svg: this.iconPack.icons.heroFire,
+        },
+        {
+            title: '<b>UX/UI</b> Lover',
+            svg: this.iconPack.icons.heroColorSwatch,
+        },
+        {
+            title: '<b>Angular</b> since v1/2010',
+            svg: this.iconPack.icons.angular,
+        },
+        {
+            title: '<b>Web</b> developer',
+            svg: this.iconPack.icons.devices,
+        },
+        {
+            title: '<b>Mobile</b> developer',
+            svg: this.iconPack.icons.heroPhone,
+        },
+        {
+            title: '<b>Full stack</b> developer',
+            svg: this.iconPack.icons.capacitor,
+        },
+        {
+            title: '<b>Community</b> member',
+            svg: this.iconPack.icons.github,
+        },
+    ];
+
+    constructor(public iconPack: IconService) {}
 
     ngOnInit(): void {}
 }
