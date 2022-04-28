cc6fee45116cc5a7b62ea92cf211093b13e0a807
Matheus Davidson <matheusdavidson@gmail.com>
2022-04-28 17:27:10 -0300

    feat(contact): top button

--- projects/landing/src/app/modules/section-contact/section-contact.module.ts
+++ projects/landing/src/app/modules/section-contact/section-contact.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionContactComponent } from './section-contact.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [SectionContactComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     exports: [SectionContactComponent],
 })
 export class SectionContactModule {}
